868e59c05214b993161f7caed446abb40f3deac6
taesamja <taesamja12345@gmail.com>
2025-02-28 22:29:52 +0900

    ESP32 Frame
    부산전자공고 전기전자과 3학년 응용프로그래밍개발 Wokwi 플랫폼과 VSCode연동을 위한 기본 틀

---
+++ ESP32Proj/src/main.ino
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+#define LED_RED 25
+#define LED_YELLOW 26
+#define LED_GREEN 27
+
+void setup() {
+    pinMode(LED_RED, OUTPUT);
+    pinMode(LED_YELLOW, OUTPUT);
+    pinMode(LED_GREEN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_RED, HIGH); delay(500);
+    digitalWrite(LED_RED, LOW); delay(500);
+    digitalWrite(LED_YELLOW, HIGH); delay(500);
+    digitalWrite(LED_YELLOW, LOW); delay(500);
+    digitalWrite(LED_GREEN, HIGH); delay(500);
+    digitalWrite(LED_GREEN, LOW); delay(500);
+}
